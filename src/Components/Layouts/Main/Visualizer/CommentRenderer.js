677ffe354b70a6df384fc22dad48c2e6dc170bb3
mattiabiondi <mattiabiondi1@gmail.com>
2019-02-03 18:56:23 +0100

    comments' likes only shown if > 0

--- src/Components/Layouts/Main/Visualizer/CommentRenderer.js
+++ src/Components/Layouts/Main/Visualizer/CommentRenderer.js
@@ -28,33 +28,57 @@ class CommentRenderer extends Component {
   render() {
     const { classes } = this.props
 
-    return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label = "Avatar"
-              alt = "avatar"
-              src = {this.props.avatar}
-            />
-          }
-          title = {this.props.author}
-          subheader = {this.props.time}
-        />
-        <CardContent>
-          <Typography component="p">
-            {this.props.text}
-          </Typography>
-        </CardContent>
-        <CardActions className={classes.actions}>
-          <Icon>
-            <LikeIcon />
-          </Icon>
-          <Typography className={classes.likes}>
-            {this.props.likes}
-          </Typography>
-        </CardActions>
-      </Card>
-    )
+    if(this.props.likes > 0) {
+      return (
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label = "Avatar"
+                alt = "avatar"
+                src = {this.props.avatar}
+              />
+            }
+            title = {this.props.author}
+            subheader = {this.props.time}
+          />
+          <CardContent>
+            <Typography component="p">
+              {this.props.text}
+            </Typography>
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Icon>
+              <LikeIcon />
+            </Icon>
+            <Typography className={classes.likes}>
+              {this.props.likes}
+            </Typography>
+          </CardActions>
+        </Card>
+      )
+    }
+    else {
+      return (
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label = "Avatar"
+                alt = "avatar"
+                src = {this.props.avatar}
+              />
+            }
+            title = {this.props.author}
+            subheader = {this.props.time}
+          />
+          <CardContent>
+            <Typography component="p">
+              {this.props.text}
+            </Typography>
+          </CardContent>
+        </Card>
+      )
+    }
+
   }
 }
 
