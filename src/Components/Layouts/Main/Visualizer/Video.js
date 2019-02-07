c613e44249a3b66f7d37a75049ba467af44f0662
Julien <gaglioti.julien@gmail.com>
2019-02-07 23:33:20 +0100

    added timer to keep track of seconds watched

--- src/Components/Layouts/Main/Visualizer/Video.js
+++ src/Components/Layouts/Main/Visualizer/Video.js
@@ -5,6 +5,7 @@ import Paper from '@material-ui/core/Paper'
 import YouTube from 'react-youtube'
 import Fade from '@material-ui/core/Fade'
 
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -21,14 +22,20 @@ class Video extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      seconds: 0,
       checked: false
     }
+    this.tracking = false
+    this.startTimer = this.startTimer.bind(this)
+    this.stopTimer = this.stopTimer.bind(this)
+    this.count = this.count.bind(this)
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.id !== this.props.id) {
       this.setState(
         {
+          seconds:0,
           checked: true
         }
       )
@@ -37,7 +44,6 @@ class Video extends Component {
 
   render() {
     const { classes } = this.props
-
     const opts = {
       width: '100%',
       playerVars: { // https://developers.google.com/youtube/player_parameters
@@ -51,11 +57,38 @@ class Video extends Component {
           <YouTube
             videoId={this.props.id}
             opts={opts}
+            onPlay={
+              this.startTimer
+            }
+            onPause={
+              this.stopTimer
+            }
           />
         </Paper>
       </Fade>
     )
   }
+
+  startTimer() {
+    if (this.tracking==false){
+      this.tracking = true
+      this.timer = setInterval(this.count, 1000)
+    }
+  }
+
+  stopTimer() {
+    this.tracking = false
+    clearInterval(this.timer)
+  }
+
+  count() {
+    var sec = this.state.seconds + 1
+    this.setState({
+      seconds: sec
+    })
+    console.log(this.state.seconds)
+  }
+
 }
 
 Video.propTypes = {
